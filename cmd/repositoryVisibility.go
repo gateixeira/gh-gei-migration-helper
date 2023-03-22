46131c55f0f1e84d4d22d33f82e209944916749e
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2023-03-22 14:08:39 +0100

    Implement command to change repository visibility

--- cmd/repositoryVisibility.go
+++ cmd/repositoryVisibility.go
@@ -1,40 +1,81 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Package cmd provides a command-line interface for changing GHAS settings for a given organization.
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/google/go-github/v50/github"
 	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+)
+
+const (
+	repositoryFlagName      = "repo"
+	visibilityFlagName = "visibility"
 )
 
 // repositoryVisibilityCmd represents the repositoryVisibility command
 var repositoryVisibilityCmd = &cobra.Command{
 	Use:   "repositoryVisibility",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Change the visibility of a repository",
+	//Long: `Change the visibility of a repository.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("repositoryVisibility called")
+		repository, err := cmd.Flags().GetString(repositoryFlagName)
+		if err != nil {
+			log.Fatalf("failed to get repository flag value: %v", err)
+		}
+		visibility, err := cmd.Flags().GetString(visibilityFlagName)
+		if err != nil {
+			log.Fatalf("failed to get activate flag value: %v", err)
+		}
+		token, err := cmd.Flags().GetString("token")
+		if err != nil {
+			log.Fatalf("failed to get token flag value: %v", err)
+		}
+
+		changerepositoryVisibility(repository, visibility, token)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(repositoryVisibilityCmd)
 
-	// Here you will define your flags and configuration settings.
+	repositoryVisibilityCmd.Flags().String(repositoryFlagName, "", "The repository to change visibility for. Format: owner/repo")
+	repositoryVisibilityCmd.MarkFlagRequired(repositoryFlagName)
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// repositoryVisibilityCmd.PersistentFlags().String("foo", "", "A help for foo")
+	repositoryVisibilityCmd.Flags().String(visibilityFlagName, "", "The new visibility. <private|internal|public>")
+	repositoryVisibilityCmd.MarkFlagRequired(visibilityFlagName)
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// repositoryVisibilityCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func changerepositoryVisibility(repository string, visibility string, token string) {
+	fmt.Println("Changing visibility for repository " + repository + " to " + visibility)
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	//create new repository object
+	newRepoSettings := github.Repository{
+		Visibility: &visibility,
+	}
+
+	// Update the repository
+	_, _, err := client.Repositories.Edit(ctx, strings.Split(repository, "/")[0], strings.Split(repository, "/")[1], &newRepoSettings)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
